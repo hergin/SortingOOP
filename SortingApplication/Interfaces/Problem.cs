f85ff6375845abe09669b6d16962a57ad6488dee
hergin <hergin@crimson.ua.edu>
2015-10-28 17:27:23 -0500

    -some refactoring

--- SortingApplication/Interfaces/Problem.cs
+++ SortingApplication/Interfaces/Problem.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SortingApplication
+namespace SortingApplication.Interfaces
 {
     public interface Problem
     {
--- SortingApplication/ProblemImplementations/AnActualProblem.cs
+++ SortingApplication/ProblemImplementations/AnActualProblem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SortingApplication.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
