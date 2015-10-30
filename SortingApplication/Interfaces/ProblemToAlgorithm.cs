0a2e750c23c4039229c5ccdf72cf4e941b46a2d8
hergin <hergin@crimson.ua.edu>
2015-10-29 22:50:09 -0500

    -comment added

--- SortingApplication/Interfaces/ProblemToAlgorithm.cs
+++ SortingApplication/Interfaces/ProblemToAlgorithm.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace SortingApplication.Interfaces
 {
+    /*
+        I had to make this a class, since C# doesn't
+        let anonymous classes to implement an interface,
+        furthermore it doesn't let anonymous classes.
+    */
     public class ProblemToAlgorithm
     {
         List<int> data;
